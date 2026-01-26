4c8d5b86ca2e62e1c56bae4a66a238d77ddd1d9a
venkatsundaraj <venkateshsundarasan@gmail.com>
2026-01-26 11:33:24 +0530

    local-app-1

--- apps/tan/src/lib/auth-client.ts
+++ apps/tan/src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.VITE_HONO_URL,
+  baseURL: "https://api.venkateshsundarasan.workers.dev/",
   fetchOptions: {
     credentials: "include",
   },
