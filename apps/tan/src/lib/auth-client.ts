6e669a02f37f942938c5b4af6b63876716a69ed2
venkatsundaraj <venkateshsundarasan@gmail.com>
2026-01-26 11:23:17 +0530

    local-app-1

--- apps/tan/src/lib/auth-client.ts
+++ apps/tan/src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.VITE_APP_URL! as string,
   fetchOptions: {
     credentials: "include",
   },
